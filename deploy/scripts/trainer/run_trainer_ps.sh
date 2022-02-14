196b2e6f1b54d8e68cd19a0981812eb1efeee382
Li Xiaoguang <lixiaoguang.01@bytedance.com>
2022-02-14 18:20:17 +0800

    fix(sgx): add mock tf io py to non-sgx entry script

--- deploy/scripts/trainer/run_trainer_ps.sh
+++ deploy/scripts/trainer/run_trainer_ps.sh
@@ -20,4 +20,6 @@ export CUDA_VISIBLE_DEVICES=
 source /app/deploy/scripts/hdfs_common.sh || true
 source /app/deploy/scripts/pre_start_hook.sh || true
 
+cp /app/sgx/gramine/CI-Examples/tensorflow_io.py ./
+
 python -m fedlearner.trainer.parameter_server $POD_IP:50051 
